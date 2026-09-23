df0a4a3674855657744f5bc46a17838a542ca61b
Mathys ARNAL <mathys.arnal@iut-rodez.fr>
2026-09-23 20:28:59 +0200

    Fin de l'étape 2 et avancement étape 3 (seulement les long)

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -1,8 +1,6 @@
 public class Utils {
 
-    public static writeInt writeInt(byte[] memory, int offset, int value) {
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
-        // à partir de 'offset', en big-endian.
+    public static int writeInt (byte[] memory, int offset, int value) {
         memory[offset] = (byte) (value >> 24);
         memory[offset + 1] = (byte) (value >> 16);
         memory[offset + 2] = (byte) (value >> 8);
@@ -12,34 +10,77 @@ public class Utils {
     }
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
-        int v1,
-            v2,
-            v3,
-            v4
-            valeurFinale;
-        v1 = (int) memory[offset + 3] (v1);
-        v1 = v1 << 24;
-        v2 = (int) memory[offset + 2] (v2);
-        v2 = v2 << 16;
-        v3 = (int) memory[offset + 1] (v3);
-        v3 = v3 << 8;
-        v4 = (int) memory[offset] (v4);
+        int v1 = (memory[offset] & 0xFF) << 24;
+        int v2 = (memory[offset + 1] & 0xFF) << 16;
+        int v3 = (memory[offset + 2] & 0xFF) << 8;
+        int v4 = (memory[offset + 3] & 0xFF);
 
-        valeurFinale = v1 | v2 | v3 | v4;
-        return valeurFinale;
+        return v1 | v2 | v3 | v4;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
-        // TODO: Écrire les 2 octets de 'value'.
         memory[offset] = (byte) (value >> 8);
         memory[offset + 1] = (byte) (value);
 
-        return memory;
+        return 2;
     }
 
     public static short readShort(byte[] memory, int offset) {
-        // TODO: Lire le short sur 2 octets.
-        return 0;
+        int v1 = (memory[offset] & 0xFF) << 8;
+        int v2 = (memory[offset + 1] & 0xFF);
+
+        return (short) (v1 | v2);
+    }
+
+    public static int writeLong(byte[] memory, int offset, long value) {
+        memory[offset] = (byte) (value >> 56);
+        memory[offset + 1] = (byte) (value >> 48);
+        memory[offset + 2] = (byte) (value >> 40);
+        memory[offset + 3] = (byte) (value >> 32);
+        memory[offset + 4] = (byte) (value >> 24);
+        memory[offset + 5] = (byte) (value >> 16);
+        memory[offset + 6] = (byte) (value >> 8);
+        memory[offset + 7] = (byte) (value);
+
+        return 8;
+    }
+
+    public static long readLong(byte[] memory, int offset) {
+        long v1 = ((long) memory[offset] & 0xFF) << 56;
+        long v2 = ((long) memory[offset + 1] & 0xFF) << 48;
+        long v3 = ((long) memory[offset + 2] & 0xFF) << 40;
+        long v4 = ((long) memory[offset + 3] & 0xFF) << 32;
+        long v5 = ((long) memory[offset + 4] & 0xFF) << 24;
+        long v6 = ((long) memory[offset + 5] & 0xFF) << 16;
+        long v7 = ((long) memory[offset + 6] & 0xFF) << 8;
+        long v8 = ((long) memory[offset + 7] & 0xFF);
+
+        return v1 | v2 | v3 | v4 | v5 | v6 | v7 | v8;
+    }
+
+    public static int writeString(
+        byte[] memory,
+        int offset,
+        String str,
+        int maxLength) {
+
+        // TODO:
+        // 1. Convertir la chaîne en octets.
+        // 2. Copier les octets sans dépasser maxLength.
+        // 3. Nettoyer le reste de la zone avec des zéros.
+
+        return maxLength;
+    }
+
+    public static String readString(
+            byte[] memory,
+            int offset,
+            int maxLength) {
+
+        // TODO:
+        // Lire jusqu'au premier octet nul
+        // ou jusqu'à maxLength.
+
+        return "";
     }
 }
